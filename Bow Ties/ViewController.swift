36dbbff70623fb81abf78af56645562d6d26f0c2
HaiVL <hai.softech@gmail.com>
2017-11-13 10:22:56 +0700

    feature - read plist and map save onto data

--- Bow Ties/ViewController.swift
+++ Bow Ties/ViewController.swift
@@ -21,6 +21,7 @@
  */
 
 import UIKit
+import CoreData
 
 final class ViewController: UIViewController {
 
@@ -33,9 +34,99 @@ final class ViewController: UIViewController {
   @IBOutlet private weak var lastWornLabel: UILabel!
   @IBOutlet private weak var favoriteLabel: UILabel!
 
+  // MARK: - Properties
+  var managedContext: NSManagedObjectContext!
+
   // MARK: - View Life Cycle
   override func viewDidLoad() {
     super.viewDidLoad()
+
+    insertSampleData()
+
+    request()
+  }
+
+  // MARK: - Private Func
+  private func request() {
+
+    let request = NSFetchRequest<Bowtie>(entityName: "Bowtie")
+    let firstTitle = segmentedControl.titleForSegment(at: 0)!
+    request.predicate = NSPredicate(format: "searchKey == %@", firstTitle)
+
+    do {
+      let results = try managedContext.fetch(request)
+      populate(bowtie: results.first!)
+
+    } catch let error as NSError {
+      print("Could not fetch \(error), \(error.userInfo)")
+    }
+
+  }
+
+  private func populate(bowtie: Bowtie) {
+    guard let imageData = bowtie.photoData as? Data,let lastWorn = bowtie.lastWorn as? Date,
+      let tintColor = bowtie.tinColor as? UIColor else {
+        return
+    }
+
+    imageView.image = UIImage(data: imageData)
+    nameLabel.text = bowtie.name
+    ratingLabel.text = "Rating: \(bowtie.rating)/5"
+
+    timesWornLabel.text = "# times worn: \(bowtie.timesWorn)"
+
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateStyle = .short
+    dateFormatter.timeStyle = .none
+
+    lastWornLabel.text = "Last worn: " + dateFormatter.string(from: lastWorn)
+
+    favoriteLabel.isHidden = !bowtie.isFavorite
+    view.tintColor = tintColor
+
+  }
+
+  private func insertSampleData() {
+    let fetch = NSFetchRequest<Bowtie>(entityName: "Bowtie")
+    fetch.predicate = NSPredicate(format: "searchKey != nil")
+
+    let count = try! managedContext.count(for: fetch)
+    if count > 0 {
+      return
+    }
+
+    // get plist
+    let path = Bundle.main.path(forResource: "SampleData", ofType: "plist")
+    let dataArray = NSArray(contentsOfFile: path!)!
+
+    for dict in dataArray {
+      let entity = NSEntityDescription.entity(forEntityName: "Bowtie", in: managedContext)!
+      let bowtie = Bowtie(entity: entity, insertInto: managedContext)
+
+      let btDict = dict as! [String: AnyObject]
+
+      bowtie.name = btDict["name"] as? String
+      bowtie.searchKey = btDict["searchKey"] as? String
+      bowtie.rating = btDict["rating"] as! Double
+
+      let colorDict = btDict["tintColor"] as! [String: AnyObject]
+      bowtie.tinColor = UIColor.color(dict: colorDict)
+
+      let imageName = btDict["imageName"] as? String
+      let image = UIImage(named: imageName!)
+      let photoData = UIImagePNGRepresentation(image!)!
+      bowtie.photoData = NSData(data: photoData)
+
+      bowtie.lastWorn = btDict["lastWorn"] as? NSDate
+
+      let timesNumber = btDict["timesWorn"] as! NSNumber
+      bowtie.timesWorn = timesNumber.int32Value
+
+      bowtie.isFavorite = btDict["isFavorite"] as! Bool
+
+      try! managedContext.save()
+      
+    }
   }
 
   // MARK: - IBActions
@@ -49,5 +140,15 @@ final class ViewController: UIViewController {
   
   @IBAction private func rate(_ sender: AnyObject) {
 
+  }
+}
+
+private extension UIColor {
+  static func color(dict: [String: AnyObject]) -> UIColor? {
+    guard let red = dict["red"] as? NSNumber, let green = dict["green"] as? NSNumber, let blue = dict["blue"] as? NSNumber  else {
+      return nil
+    }
+
+    return UIColor(red: CGFloat(red)/255.0, green: CGFloat(green)/255.0, blue: CGFloat(blue)/255.0, alpha: 1)
   }
 }
